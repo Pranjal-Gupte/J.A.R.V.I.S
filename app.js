8d26cc8b794f9f56cde6a0c2bfc1b17bc33d8107
Pranjal <pranjalagupte55555@gmail.com>
2023-01-12 21:37:32 +0530

    fixed url problem

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const microphoneButton = document.getElementById("microphoneButton");
 
 microphoneButton.addEventListener("click", () => {
-    window.open(`http://127.0.0.1:5500/mic/index1.html`, "", "width=380px,height=390px");
+    window.open(`http://127.0.0.1:5500/dist/index1.html`, "", "width=380px,height=390px");
 });
